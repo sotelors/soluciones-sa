f42a402962052ae2bc27070c1c49ce646cdb5019
sotelors <rubenalfonsonic@hotmail.com>
2020-11-06 23:58:32 -0600

    Updates

---
+++ precache-manifest.f4da66f8215c75b1455939b7d3f5239b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5745cc6652fb2bb226266f58fe51e3f2",
+    "url": "/soluciones-sa/index.html"
+  },
+  {
+    "revision": "9d54a9ab3eeee6164434",
+    "url": "/soluciones-sa/static/css/main.8438516f.chunk.css"
+  },
+  {
+    "revision": "8d6ab8841f7326317121",
+    "url": "/soluciones-sa/static/js/2.4d3b8b15.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/soluciones-sa/static/js/2.4d3b8b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d54a9ab3eeee6164434",
+    "url": "/soluciones-sa/static/js/main.06de42e4.chunk.js"
+  },
+  {
+    "revision": "0c9b97042d6fc1f58ae2",
+    "url": "/soluciones-sa/static/js/runtime-main.057cfdb3.js"
+  }
+]);
